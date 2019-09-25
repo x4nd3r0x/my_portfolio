513dacc2d88d884eae264e9e91f0237624c80c4f
x4nd3r0x <aitelfakih654hamza@gmail.com>
2019-09-25 17:20:50 +0100

    scroll up button

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,5 @@
+//menu button start
+
 //select DOM Item
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
@@ -31,3 +33,32 @@ function toggleMenu() {
         showMenu = false;
     }
 }
+
+//menu button end
+
+//scroll up button hide start
+
+
+//Get the button:
+mybutton = document.getElementById("topbtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+//scroll up button hide end
